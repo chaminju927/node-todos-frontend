beba0e9260e61cee72771bdbf131d5539b4b891c
chaminju <minjoo92718@gmail.com>
2024-10-22 00:23:02 +0900

    Test

--- src/App.js
+++ src/App.js
@@ -1,67 +1,18 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import TodoBoard from "./components/TodoBoard";
-
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import { useEffect, useState } from "react";
-import api from "./utils/api";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import TodoPage from "./pages/TodoPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [todoValue, setTodoValue] = useState("");
-
-  const getTasks = async () => {
-    const response = await api.get("/tasks");
-    console.log("res:", response);
-    setTodoList(response.data.data);
-  };
-
-  const addTask = async () => {
-    try {
-      const response = await api.post("/tasks", {
-        task: todoValue,
-        isComplete: false,
-      });
-      if (response.status === 200) {
-        console.log("success");
-        setTodoValue("");
-        getTasks();
-      } else {
-        throw new Error("task cant be added");
-      }
-    } catch (err) {
-      console.log("error", err);
-    }
-  };
-
-  useEffect(() => {
-    getTasks();
-  }, []);
-
   return (
-    <Container>
-      <Row className="add-item-row">
-        <Col xs={12} sm={10}>
-          <input
-            type="text"
-            placeholder="할일을 입력하세요"
-            className="input-box"
-            value={todoValue}
-            onChange={(event) => setTodoValue(event.target.value)}
-          />
-        </Col>
-        <Col xs={12} sm={2}>
-          <button className="button-add" onClick={addTask}>
-            추가
-          </button>
-        </Col>
-      </Row>
+    <Routes>
+      <Route path="/" element={<TodoPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
-      <TodoBoard todoList={todoList} getTasks={getTasks} />
-    </Container>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
